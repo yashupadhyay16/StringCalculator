66556e47ba960bbe690f7a8d837b0f089b09e18e
Yash Upadhyay <upadhyayyash923@gmail.com>
2024-05-22 01:37:18 +0530

    [SC] refactored class StringCaluclator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,25 +1,23 @@
 # frozen_string_literal: true
-require 'pry'
+
 # Class representing a simple string calculator.
 class StringCalculator
   MAXIMUM_NUMBER = 1000
-
+  # Adds the numbers provided in the input string.
+  #
+  # @param numbers [String] A string containing comma-separated numbers.
+  # @return [Integer] The sum of the numbers.
   def add(numbers)
     return 0 if numbers.empty?
 
     delimiter, numbers = extract_delimiter(numbers)
-    numbers_array = numbers.split(/#{delimiter}|\n/).map(&:to_i)
-    
-    negative_numbers = numbers_array.select { |num| num.negative? }
-    if negative_numbers.any?
-      raise ArgumentError, "Negative numbers not allowed: #{negative_numbers.join(', ')}"
-    end
+    numbers_array = parse_input(numbers, delimiter)
+    validate_numbers(numbers_array)
 
     numbers_array.reject! { |num| num > MAXIMUM_NUMBER }
 
     numbers_array.sum
   end
-
 
   private
 
@@ -27,9 +25,17 @@ class StringCalculator
     delimiter = ','
     if numbers.start_with?('//')
       delimiter_line, numbers = numbers.split("\n", 2)
-
       delimiter = delimiter_line[2..]
     end
     [delimiter, numbers]
+  end
+
+  def parse_input(numbers, delimiter)
+    numbers.split(/#{delimiter}|\n/).map(&:to_i)
+  end
+
+  def validate_numbers(numbers_array)
+    negative_numbers = numbers_array.select(&:negative?)
+    raise ArgumentError, "Negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
   end
 end
