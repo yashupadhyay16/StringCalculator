61f149b4b65f34aa36ede9e212dd8f23d54bf75e
Yash Upadhyay <upadhyayyash923@gmail.com>
2024-05-22 00:40:46 +0530

    [SC] Add spec to support different delimator

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -38,5 +38,11 @@ RSpec.describe StringCalculator do
         expect(calculator.add("1\n2,3")).to eq(6)
       end
     end
+
+    context 'with a custom delimiter' do
+      it 'returns the sum' do
+        expect(calculator.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
