91cbfdc9db1074300adfb7ea239b3edba14dda9a
Yash Upadhyay <upadhyayyash923@gmail.com>
2024-07-15 18:23:52 +0530

    [SC] Update method to add numbers having custom delimator

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -6,6 +6,10 @@ class StringCalculator
     return 0 if numbers.empty?
 
     delimiter = ','
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers[2], numbers.split("\n", 2).last
+    end
+    
     numbers.gsub("\n", delimiter).split(delimiter).map(&:to_i).sum
   end
 end
